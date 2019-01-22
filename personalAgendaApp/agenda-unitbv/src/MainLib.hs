e207b783b8c6303e10939ee9a1e58f7fc8984c78
Florin Asavei <asaveiflorin@gmail.com>
2019-01-22 21:05:20 +0200

    commit

--- personalAgendaApp/agenda-unitbv/src/MainLib.hs
+++ personalAgendaApp/agenda-unitbv/src/MainLib.hs
@@ -7,12 +7,16 @@ mainPrompt = do
     putStrLn "Enter your option"
     putStrLn "1 - Create New Agenda"
     putStrLn "2 - Load Agenda"
-    putStrLn "3 -  Delete Agenda"
+    putStrLn "3 - Delete Agenda"
     userOption <- getLine
-    --performPromtAction userOption
-    return userOption
+    let actionResult = performPromtAction  userOption
+    return actionResult
 
-
-
+performPromtAction  :: String -> String
 performPromtAction userOption = case (userOption) of
-    "1" -> "Creating new agenda"
+    "1" -> 
+         "Creating new agenda"
+    "2" -> 
+         "Loading Agenda"
+
+        
